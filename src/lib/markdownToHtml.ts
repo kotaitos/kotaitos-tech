91764947c892f44cf9101cc03d411fec6fd9ab0b
IAMKOTARO <saikota0314@gmail.com>
2024-05-26 22:26:14 +0900

    すべてのaタグにtarget='_blank'を設定

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -4,6 +4,9 @@ import { unified } from 'unified';
 import rehypeParse from 'rehype-parse';
 import rehypeStringify from 'rehype-stringify';
 import rehypeHighlight from 'rehype-highlight';
+import { visit } from 'unist-util-visit';
+import { Node } from 'unist';
+import { Element } from 'hast';
 
 export default async function markdownToHtml(
   markdown: string,
@@ -14,6 +17,15 @@ export default async function markdownToHtml(
     .use(rehypeParse, { fragment: true })
     .use(rehypeHighlight)
     .use(rehypeStringify)
+    .use(() => {
+      return (tree: Node) => {
+        visit(tree, 'element', (node: Element) => {
+          if (node.tagName === 'a') {
+            node.properties = { ...node.properties, target: '_blank' };
+          }
+        });
+      };
+    })
     .process(markdownToHtmlResult.toString());
 
   return highlightedHtml.toString();
