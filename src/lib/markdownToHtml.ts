32ffefda524c4125c48e3702264ab08c33527642
IAMKOTARO <saikota0314@gmail.com>
2024-05-26 20:41:56 +0900

    :recycle:コメントを削除

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -8,10 +8,8 @@ import rehypeHighlight from 'rehype-highlight';
 export default async function markdownToHtml(
   markdown: string,
 ): Promise<string> {
-  // MarkdownをHTMLに変換
   const markdownToHtmlResult = await remark().use(html).process(markdown);
 
-  // シンタックスハイライトを適用
   const highlightedHtml = await unified()
     .use(rehypeParse, { fragment: true })
     .use(rehypeHighlight)
