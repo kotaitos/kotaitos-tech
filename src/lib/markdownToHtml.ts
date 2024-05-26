0cf2a50af93a312369fb4e4c8046d508938ee13c
IAMKOTARO <saikota0314@gmail.com>
2024-05-26 20:29:01 +0900

    hilightingを導入

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,22 @@
 import { remark } from 'remark';
 import html from 'remark-html';
+import { unified } from 'unified';
+import rehypeParse from 'rehype-parse';
+import rehypeStringify from 'rehype-stringify';
+import rehypeHighlight from 'rehype-highlight';
 
-export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
-  return result.toString();
+export default async function markdownToHtml(
+  markdown: string,
+): Promise<string> {
+  // MarkdownをHTMLに変換
+  const markdownToHtmlResult = await remark().use(html).process(markdown);
+
+  // シンタックスハイライトを適用
+  const highlightedHtml = await unified()
+    .use(rehypeParse, { fragment: true })
+    .use(rehypeHighlight)
+    .use(rehypeStringify)
+    .process(markdownToHtmlResult.toString());
+
+  return highlightedHtml.toString();
 }
